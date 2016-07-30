f492f6c7c29d51b5774d0f9c36c065e8086cf496
Ankit G <ankit31894@gmail.com>
2016-07-30 05:35:24 +0530

    removed bug when 0 missing in decimal number

--- dist/browser/math-expression-evaluator.js
+++ dist/browser/math-expression-evaluator.js
@@ -1,5 +1,5 @@
-/** math-expression-evaluator version 1.2.9
- Dated:2016-06-12 */
+/** math-expression-evaluator version 1.2.10
+ Dated:2016-07-30 */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mexp = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Mexp=require('./postfix_evaluator.js');
@@ -252,7 +252,8 @@ module.exports=Mexp;
 					throw(new Mexp.exception("Two decimals are not allowed in one number"));
 				}
 				if(pre.type!==1){
-					str.push({value:0,type:1,pre:0});
+					pre={value:0,type:1,pre:0};	//pre needs to be changed as it will the last value now to be safe in later code
+					str.push(pre);
 					inc(ptc,-1);
 				}
 				allowed=type6;
@@ -329,6 +330,7 @@ module.exports=Mexp;
 		}
 		for(var j=ptc.length;j--;){	//loop over ptc
 			if(ptc[j]===0){
+
 				str.push({value:")",show:")",type:5,pre:3});
 				inc(ptc,-1).pop();
 			}
@@ -344,6 +346,7 @@ module.exports=Mexp;
 		return new Mexp(str);
 	};
     module.exports=Mexp;
+
 },{"./math_function.js":3}],3:[function(require,module,exports){
 	var Mexp=function(parsed){
 		this.value=parsed;
