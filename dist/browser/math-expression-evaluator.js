3d5afe2a57e3d4f6d6d4e86e69c337174b3aacc0
Ankit <ankit31894@gmail.com>
2016-08-22 16:32:59 +0530

    Released/Created new distribution file in dist folder

--- dist/browser/math-expression-evaluator.js
+++ dist/browser/math-expression-evaluator.js
@@ -1,5 +1,5 @@
-/** math-expression-evaluator version 1.2.10
- Dated:2016-08-09 */
+/** math-expression-evaluator version 1.2.11
+ Dated:2016-08-22 */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mexp = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Mexp=require('./postfix_evaluator.js');
@@ -469,17 +469,74 @@ module.exports=Mexp;
 	};
     module.exports=Mexp;
 },{}],4:[function(require,module,exports){
-    
+
     var Mexp=require('./lexer.js');
-	if(!Array.indexOf)
-		Array.prototype.indexOf = function (vItem) {
-			for (var i=0; i < this.length; i++) {
-				if (vItem == this[i]) {
-				return i;
-				}
-			}
-		  return -1;
-		};
+	// Production steps of ECMA-262, Edition 5, 15.4.4.14
+	// Reference: http://es5.github.io/#x15.4.4.14
+	if (!Array.prototype.indexOf) {
+	  Array.prototype.indexOf = function(searchElement, fromIndex) {
+
+	    var k;
+
+	    // 1. Let o be the result of calling ToObject passing
+	    //    the this value as the argument.
+	    if (this == null) {
+	      throw new TypeError('"this" is null or not defined');
+	    }
+
+	    var o = Object(this);
+
+	    // 2. Let lenValue be the result of calling the Get
+	    //    internal method of o with the argument "length".
+	    // 3. Let len be ToUint32(lenValue).
+	    var len = o.length >>> 0;
+
+	    // 4. If len is 0, return -1.
+	    if (len === 0) {
+	      return -1;
+	    }
+
+	    // 5. If argument fromIndex was passed let n be
+	    //    ToInteger(fromIndex); else let n be 0.
+	    var n = +fromIndex || 0;
+
+	    if (Math.abs(n) === Infinity) {
+	      n = 0;
+	    }
+
+	    // 6. If n >= len, return -1.
+	    if (n >= len) {
+	      return -1;
+	    }
+
+	    // 7. If n >= 0, then Let k be n.
+	    // 8. Else, n<0, Let k be len - abs(n).
+	    //    If k is less than 0, then let k be 0.
+	    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+	    // 9. Repeat, while k < len
+	    while (k < len) {
+	      // a. Let Pk be ToString(k).
+	      //   This is implicit for LHS operands of the in operator
+	      // b. Let kPresent be the result of calling the
+	      //    HasProperty internal method of o with argument Pk.
+	      //   This step can be combined with c
+	      // c. If kPresent is true, then
+	      //    i.  Let elementK be the result of calling the Get
+	      //        internal method of o with the argument ToString(k).
+	      //   ii.  Let same be the result of applying the
+	      //        Strict Equality Comparison Algorithm to
+	      //        searchElement and elementK.
+	      //  iii.  If same is true, return k.
+	      if (k in o && o[k] === searchElement) {
+	        return k;
+	      }
+	      k++;
+	    }
+	    return -1;
+	  };
+	}
+
 	Mexp.prototype.toPostfix = function () {
 		'use strict';
 		var post=[],elem,popped,prep,pre,ele;
